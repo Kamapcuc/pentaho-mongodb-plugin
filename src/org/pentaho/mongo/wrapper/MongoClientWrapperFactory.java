8711b741aea75bc17a0773a12528aa04dc700c1c
Gregory <Gregory.Bityukov@gmail.com>
2014-08-28 18:14:26 +0400

    fix empty user/password

--- src/org/pentaho/mongo/wrapper/MongoClientWrapperFactory.java
+++ src/org/pentaho/mongo/wrapper/MongoClientWrapperFactory.java
@@ -1,5 +1,6 @@
 package org.pentaho.mongo.wrapper;
 
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
 import org.pentaho.di.core.Const;
@@ -9,16 +10,25 @@ import org.pentaho.di.core.variables.VariableSpace;
 import org.pentaho.di.trans.steps.mongodb.MongoDbMeta;
 
 public class MongoClientWrapperFactory {
-  public static MongoClientWrapper createMongoClientWrapper( MongoDbMeta meta, VariableSpace vars, LogChannelInterface log )
-    throws KettleException {
-    if ( meta.getUseKerberosAuthentication() ) {
-      KerberosMongoClientWrapper wrapper = new KerberosMongoClientWrapper( meta, vars, log );
-      return (MongoClientWrapper) Proxy.newProxyInstance( wrapper.getClass().getClassLoader(),
-          new Class<?>[] { MongoClientWrapper.class }, new KerberosInvocationHandler( wrapper.getAuthContext(), wrapper ) );
-    } else if ( !Const.isEmpty( meta.getAuthenticationUser() ) || !Const.isEmpty( meta.getAuthenticationPassword() ) ) {
-      return new UsernamePasswordMongoClientWrapper( meta, vars, log );
-    } else {
-      return new NoAuthMongoClientWrapper( meta, vars, log );
+
+    public static MongoClientWrapper createMongoClientWrapper(MongoDbMeta meta, VariableSpace vars,
+                                                              LogChannelInterface log) throws KettleException {
+
+        if (meta.getUseKerberosAuthentication()) {
+            KerberosMongoClientWrapper wrapper = new KerberosMongoClientWrapper(meta, vars, log);
+            ClassLoader loader = wrapper.getClass().getClassLoader();
+            Class<?>[] interfaces = new Class<?>[]{MongoClientWrapper.class};
+            InvocationHandler h = new KerberosInvocationHandler(wrapper.getAuthContext(), wrapper);
+            return (MongoClientWrapper) Proxy.newProxyInstance(loader, interfaces, h);
+        } else {
+            String user = vars.environmentSubstitute(meta.getAuthenticationUser());
+            String password = vars.environmentSubstitute(meta.getAuthenticationPassword());
+            if (!Const.isEmpty(user) || !Const.isEmpty(password)) {
+                return new UsernamePasswordMongoClientWrapper(meta, vars, log);
+            } else {
+                return new NoAuthMongoClientWrapper(meta, vars, log);
+            }
+        }
     }
-  }
+
 }
